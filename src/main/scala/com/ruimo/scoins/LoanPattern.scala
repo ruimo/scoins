9f6b66eec10f46d0fe5ce5f92eb982f1d5bfbe46
Shisei Hanai <ruimo.uno@gmail.com>
2014-09-21 15:38:25 +0900

    Can access reader as iterator.

--- src/main/scala/com/ruimo/scoins/LoanPattern.scala
+++ src/main/scala/com/ruimo/scoins/LoanPattern.scala
@@ -1,10 +1,12 @@
 package com.ruimo.scoins
 
 import scala.util.{Try, Success, Failure}
+import scala.annotation.tailrec
 import java.io.Closeable
+import java.io.Reader
 
 object LoanPattern {
-  def withResource[T <: Closeable, U](resource: T)(f: T => U): Try[U] =
+  def withResource[T <% Closeable, U](resource: T)(f: T => U): Try[U] =
     Try(f(resource)) match {
       case Success(v) => Try {
         resource.close()
@@ -20,5 +22,31 @@ object LoanPattern {
           fail.asInstanceOf[Failure[U]]
         }
       }
+    }
+
+  def iteratorFromReader[T <% Reader, U](resource: T)(f: Iterator[Char] => U): Try[U] =
+    withResource(resource) { reader =>
+      f(new Iterator[Char] {
+        var ch: Int = -2 // -2 means buffer empty
+
+        def hasNext: Boolean = {
+          if (ch == -1) false
+          else if (ch == -2) {
+            ch = reader.read()
+          }
+
+          0 <= ch
+        }
+
+        def next(): Char = if (hasNext) {
+          try {
+            ch.asInstanceOf[Char]
+          }
+          finally {
+            ch = -2
+          }
+        }
+        else throw new NoSuchElementException
+      })
     }
 }
--- src/test/scala/com/ruimo/scoins/LoanPatternSpec.scala
+++ src/test/scala/com/ruimo/scoins/LoanPatternSpec.scala
@@ -2,6 +2,7 @@ package com.ruimo.scoins
 
 import java.io.Closeable
 import java.io.IOException
+import java.io.{Reader, StringReader}
 import scala.util.{Try, Success, Failure}
 import org.specs2.mutable.Specification
 
@@ -51,6 +52,103 @@ class LoanPatternSpec extends Specification {
           f.exception.getSuppressed()(0).getMessage === "World"
         }
       }
+      1 === 1
+    }
+
+    "access empty reader" in {
+      import LoanPattern.iteratorFromReader
+
+      val r: Reader = new StringReader("")
+      iteratorFromReader(r) { z: Iterator[Char] =>
+        z.hasNext === false
+      }.isSuccess === true
+    }
+
+    "access reader" in {
+      import LoanPattern.iteratorFromReader
+
+      val r: Reader = new StringReader("abc")
+      iteratorFromReader(r) { z: Iterator[Char] =>
+        z.hasNext === true
+        z.next() === 'a'
+
+        z.hasNext === true
+        z.next() === 'b'
+
+        z.hasNext === true
+        z.next() === 'c'
+
+        z.hasNext === false
+      }.isSuccess === true
+    }
+
+    "access error in reading the first char" in {
+      val resource = mock(classOf[Reader])
+      when(resource.read()).thenThrow(new IOException("Error"))
+
+      import LoanPattern.iteratorFromReader
+
+      iteratorFromReader(resource) { z: Iterator[Char] =>
+        z.hasNext
+      } match {
+        case Success(_) => failure
+        case f: Failure[_] => {
+          f.exception.getMessage === "Error"
+        }
+      }
+
+      verify(resource).close()
+      1 === 1
+    }
+
+    "access error in reading the second char" in {
+      val resource = mock(classOf[Reader])
+      when(resource.read())
+        .thenReturn('a'.asInstanceOf[Int])
+        .thenThrow(new IOException("Error"))
+
+      import LoanPattern.iteratorFromReader
+
+      iteratorFromReader(resource) { z: Iterator[Char] =>
+        z.hasNext === true
+        z.next === 'a'
+
+        z.hasNext
+      } match {
+        case Success(_) => failure
+        case f: Failure[_] => {
+          f.exception.getMessage === "Error"
+        }
+      }
+
+      verify(resource).close()
+      1 === 1
+    }
+
+    "access error and close error in reading the second char" in {
+      val resource = mock(classOf[Reader])
+      when(resource.read())
+        .thenReturn('a'.asInstanceOf[Int])
+        .thenThrow(new IOException("Error"))
+      when(resource.close())
+        .thenThrow(new IOException("Close"))
+
+      import LoanPattern.iteratorFromReader
+
+      iteratorFromReader(resource) { z: Iterator[Char] =>
+        z.hasNext === true
+        z.next === 'a'
+
+        z.hasNext
+      } match {
+        case Success(_) => failure
+        case f: Failure[_] => {
+          f.exception.getMessage === "Close"
+          f.exception.getSuppressed().length === 1
+          f.exception.getSuppressed()(0).getMessage === "Error"
+        }
+      }
+
       1 === 1
     }
   }
