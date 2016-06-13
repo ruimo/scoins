f64f830c8c644f7cf1fb16916c82221e61fad7c1
Shisei Hanai <ruimo.uno@gmail.com>
2016-06-13 10:27:55 +0900

    Add Path util.

---
+++ src/main/scala/com/ruimo/scoins/PathUtil.scala
@@ -0,0 +1,54 @@
+package com.ruimo.scoins
+
+import scala.util.{Try, Success, Failure}
+import java.nio.file.{Files, Path, FileVisitResult, SimpleFileVisitor}
+import java.nio.file.attribute.FileAttribute
+import java.nio.file.attribute.BasicFileAttributes
+import java.io.IOException
+
+object PathUtil {
+  def deleteDir(dir: Path) {
+    Files.walkFileTree(dir, new SimpleFileVisitor[Path]() {
+      override def visitFile(path: Path, attrs: BasicFileAttributes): FileVisitResult = {
+        Files.delete(path)
+        FileVisitResult.CONTINUE
+      }
+      override def postVisitDirectory(path: Path, error: IOException) = {
+        if (error == null) {
+          Files.delete(path)
+          FileVisitResult.CONTINUE
+        }
+        else throw error
+      }
+    })
+    Files.deleteIfExists(dir)
+  }
+
+  def withTempPath[U](
+    tempFactory: () => Path, tempDestructor: Path => Unit
+  )(
+    func: Path => U
+  ): Try[U] = {
+    LoanPattern.using(tempFactory())(func)(tempDestructor)
+  }
+
+  def withTempDirectory[U](
+    baseDir: Path, prefix: Option[String], attrs: FileAttribute[_]*
+  )(
+    func: Path => U
+  )(implicit
+    tempFactory: () => Path =
+      () => Files.createTempDirectory(baseDir, prefix.orNull[String], attrs: _*),
+    tempDestructor: Path => Unit = deleteDir
+  ): Try[U] = withTempPath(tempFactory, tempDestructor)(func)
+
+  def withTempFile[U](
+    prefix: Option[String], suffix: Option[String], attrs: FileAttribute[_]*
+  )(
+    func: Path => U
+  )(implicit
+    tempFactory: () => Path =
+      () => Files.createTempFile(prefix.orNull, suffix.orNull, attrs: _*),
+    tempDestructor: Path => Unit = Files.delete
+  ): Try[U] = withTempPath(tempFactory, tempDestructor)(func)
+}
