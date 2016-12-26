120514fe7a1e447dd22731e4637f856ab462f995
Shisei Hanai <ruimo.uno@gmail.com>
2016-12-26 19:17:53 +0900

    Add method to serve InputStream.

--- src/main/scala/com/ruimo/scoins/ImmutableByteArray.scala
+++ src/main/scala/com/ruimo/scoins/ImmutableByteArray.scala
@@ -1,11 +1,13 @@
 package com.ruimo.scoins
 
 import java.nio.charset.Charset
+import java.io.{InputStream, ByteArrayInputStream}
 
 class ImmutableByteArray(private val table: Array[Byte]) {
   def apply(idx: Int): Byte = table(idx)
   def length: Int = table.length
   def asString(encoding: Charset): String = new String(table, encoding)
+  def inputStream: InputStream = new ByteArrayInputStream(table)
 }
 
 object ImmutableByteArray {
